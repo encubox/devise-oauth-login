8fc1164028f5dc23038c92df66f3fa99b21ef3c6
sigmaray <sigmaray@gmail.com>
2018-09-12 10:19:47 +0300

    Improve auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,36 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+  # def facebook
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+  #   else
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
 
   def failure
     redirect_to root_path
   end
+
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted? && user.valid?
+      sign_in user
+      flash.notice = "Signed in with #{request.env["omniauth.auth"].provider}!"
+      redirect_to after_sign_in_path_for(user)
+    elsif user.errors.include?(:email)
+      flash.alert = "Email address associated with your #{request.env["omniauth.auth"].provider} account had already been registered in the system"
+      redirect_to root_path
+    else
+      flash.alert = "Errror occured while signing in with #{request.env["omniauth.auth"].provider}. Please try again"
+      redirect_to root_path
+    end
+  end
+
+  alias_method :facebook, :all
+  alias_method :google_oauth2, :all
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # This helper is used in devise views but is absent in new versions of omniauth gems
+  def omniauth_authorize_path(resource_name, provider)
+    if provider.to_sym == :facebook
+      user_facebook_omniauth_authorize_path
+    elsif provider.to_sym == :google_oauth2
+      user_google_oauth2_omniauth_authorize_path
+    else
+      raise 'Wrong parameters.'
+    end
+  end
 end
